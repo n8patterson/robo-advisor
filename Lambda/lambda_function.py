f00cfab38dbf17fde8e12d790be751195b0b692d
Nathan Patterson <nathan.e.patterson@gmail.com>
2021-09-13 10:20:29 +0900

    inital commit

---
+++ Lambda/lambda_function.py
@@ -0,0 +1,190 @@
+### Required Libraries ###
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+### Functionality Helper Functions ###
+def parse_int(n):
+    """
+    Securely converts a non-integer value to integer.
+    """
+    try:
+        return int(n)
+    except ValueError:
+        return float("nan")
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    """
+    Define a result message structured as Lex response.
+    """
+    if message_content is None:
+        return {"isValid": is_valid, "violatedSlot": violated_slot}
+
+    return {
+        "isValid": is_valid,
+        "violatedSlot": violated_slot,
+        "message": {"contentType": "PlainText", "content": message_content},
+    }
+    
+def validate_data(age, investment_amount, intent_request):
+    # Validate that the user is over greater than zero and less than 65
+    if age is not None:
+        age = parse_int(age)
+        if age < 1 or age > 64:
+            return build_validation_result(
+                False,
+                "age",
+                "You should be between 0 and 65 years old to use this service, "
+                "please provide a different date of birth.",
+            )
+            
+    # Validate the investment amount, it should be >= 5000
+    if investment_amount is not None:
+        dollars = parse_int(investment_amount)
+        if dollars < 5000:
+            return build_validation_result(
+                False,
+                "investmentAmount",
+                "The amount to convert should be greater than or equal to 5000, "
+                "please provide a correct amount.",
+            )
+            
+    # A True results is returned if age or amount are valid
+    return build_validation_result(True, None, None)
+
+
+### Dialog Actions Helper Functions ###
+def get_slots(intent_request):
+    """
+    Fetch all the slots and their values from the current intent.
+    """
+    return intent_request["currentIntent"]["slots"]
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    """
+    Defines an elicit slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "ElicitSlot",
+            "intentName": intent_name,
+            "slots": slots,
+            "slotToElicit": slot_to_elicit,
+            "message": message,
+        },
+    }
+
+
+def delegate(session_attributes, slots):
+    """
+    Defines a delegate slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {"type": "Delegate", "slots": slots},
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    """
+    Defines a close slot type response.
+    """
+
+    response = {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": fulfillment_state,
+            "message": message,
+        },
+    }
+
+    return response
+
+### Intents Handlers ###
+def recommend_portfolio(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    first_name = get_slots(intent_request)["firstName"]
+    age = get_slots(intent_request)["age"]
+    investment_amount = get_slots(intent_request)["investmentAmount"]
+    risk_level = get_slots(intent_request)["riskLevel"]
+    source = intent_request["invocationSource"]
+
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots.
+    
+        # Gets all the slots
+        slots = get_slots(intent_request)
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(age, investment_amount, intent_request)
+        
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]] = None  # Cleans invalid slot
+    
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+            
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+    
+    # Switch output on risk level
+    risk_levels = {
+        'none': {'bonds': 100, 'bonds_ticker': 'AGG', 'equities': 0 , 'equities_ticker': 'SPY'},
+        'low': {'bonds': 60, 'bonds_ticker': 'AGG', 'equities': 40 , 'equities_ticker': 'SPY'},
+        'medium': {'bonds': 40, 'bonds_ticker': 'AGG', 'equities': 60 , 'equities_ticker': 'SPY'},
+        'high': {'bonds':20, 'bonds_ticker': 'AGG', 'equities': 80 , 'equities_ticker': 'SPY'}
+    }
+    
+    output = f"With {risk_level} risk you should invest {risk_levels.get(risk_level).get('bonds')}% in {risk_levels.get(risk_level).get('bonds_ticker')} bonds and you should invest {risk_levels.get(risk_level).get('equities')}% in {risk_levels.get(risk_level).get('equities_ticker')} stocks."    
+
+    # Return a message with conversion's result.
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": output,
+        },
+    )
+
+### Intents Dispatcher ###
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    intent_name = intent_request["currentIntent"]["name"]
+
+    # Dispatch to bot's intent handlers
+    if intent_name == "recommendPortfolio":
+        return recommend_portfolio(intent_request)
+
+    raise Exception("Intent with name " + intent_name + " not supported")
+
+
+### Main Handler ###
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+
+    return dispatch(event)
---
+++ README.md
@@ -0,0 +1,51 @@
+# Robo advisor with AWS Lex
+
+This project creates a robo advisor with AWS Lex and then suggests a portfolio based off of your risk.
+
+---
+
+## Technologies
+
+This project leverages python 3.7.9 with the following packages:
+
+* [AWS Lex](https://aws.amazon.com/lex/) - A service for building conversational interfaces into any application using voice and text.
+
+* [AWS Lambda](https://aws.amazon.com/lambda/) - To build the functionality for the bot.
+
+
+On the terminal, under the conda dev environment, install the following:
+
+---
+
+## Installation Guide
+
+1. Create AWS free account to access AWS Lambda and AWS Lex.
+2. Use VSCODE to view and edit lambda_function.py file.
+
+---
+
+## Examples
+
+
+---
+
+## Contributors 
+
+Nathan Patterson
+
+Email: nathan.e.patterson@gmail.com
+
+[LinkedIn](https://www.linkedin.com/in/natepatterson/)
+
+---
+
+## License
+
+MIT License
+Copyright (c) [2021] [Justine Cho]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
